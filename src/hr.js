eb742fd022f60b3ee94f80873f5cf73ae8fe44d3
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-07-24 14:27:20 +0100

    Add horizontal line

---
+++ src/hr.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHr = styled.hr`
+  margin: 50px auto;
+  width: 500px;
+  border: 2px solid black;
+`
+
+const Hr = () => <StyledHr />
+
+export default Hr
